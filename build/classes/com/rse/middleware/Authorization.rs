db7465e8769d41465896871efb5890c2940599e9
moh. afifun naily <moh.afifun46@gmail.com>
2017-01-17 12:40:51 +0700

    commit middleware source code

---
+++ build/classes/com/rse/middleware/Authorization.rs
@@ -0,0 +1,5 @@
+com.rse.middleware.NotPermittedException
+com.rse.middleware.Authorization
+com.rse.middleware.TokenNullException
+com.rse.middleware.TokenInvalidException
+com.rse.middleware.TokenEmptyException
